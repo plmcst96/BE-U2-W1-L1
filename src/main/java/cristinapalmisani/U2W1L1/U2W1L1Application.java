1eab2246294af5d7f8f0a19a6ff2204daf175ca0
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-01-08 16:17:37 +0100

    inizio esercizio

---
+++ src/main/java/cristinapalmisani/U2W1L1/U2W1L1Application.java
@@ -0,0 +1,13 @@
+package cristinapalmisani.U2W1L1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W1L1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W1L1Application.class, args);
+	}
+
+}
---
+++ src/main/java/cristinapalmisani/U2W1L1/entities/Pizzas.java
@@ -0,0 +1,17 @@
+package cristinapalmisani.U2W1L1.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class Pizzas {
+    private String name;
+    private int calories;
+    private double price;
+}
